bea4a6e06edb51d3c0f3ca835fb19a2bf303dd87
Vikas <vikask4590@gmail.com>
2024-02-19 11:42:04 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   flexsearch: true,
-  staticImage: true,
   defaultShowCopyCode: true,
 });
 
